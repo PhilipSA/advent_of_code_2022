6f6f4abd4a1c6e60779367fc42baece3f5315b5d
Philip Sandegren <philip.sandegren@visibacare.com>
2022-12-12 09:49:00 +0100

    day12 part 1

--- bin/advent_of_code_2022.dart
+++ bin/advent_of_code_2022.dart
@@ -1,6 +1,7 @@
 import 'package:advent_of_code_2022/day_1/day_1.dart';
 import 'package:advent_of_code_2022/day_10/day_10.dart';
 import 'package:advent_of_code_2022/day_11/day_11.dart';
+import 'package:advent_of_code_2022/day_12/day_12.dart';
 import 'package:advent_of_code_2022/day_2/day_2.dart';
 import 'package:advent_of_code_2022/day_3/day_3.dart';
 import 'package:advent_of_code_2022/day_4/day_4.dart';
@@ -22,4 +23,5 @@ void main(List<String> arguments) {
   day9();
   day10();
   day11();
+  day12();
 }
---
+++ lib/day_12/day_12.dart
@@ -0,0 +1,161 @@
+import 'dart:collection';
+import 'dart:math';
+
+import 'package:advent_of_code_2022/util/file_util.dart';
+
+void day12() {
+  final fileLines = getInputFileLines(12);
+
+  final part1 = day12Read(fileLines);
+  final part2 = day12Read(fileLines);
+  print("Day 12 part 1: $part1 part 2: $part2");
+}
+
+int day12Read(List<String> fileLines) {
+  final pathFinder = PathFinder.createFromFileLines(fileLines);
+  final takenPath = pathFinder.aStar(pathFinder.startNode);
+
+  var shortestPath = 100000000000;
+  for (final startNode in pathFinder.alternativeStartNodes) {
+    final pathFinderCopy = PathFinder.createFromFileLines(fileLines);
+    final startNodeInCopy = pathFinderCopy.alternativeStartNodes.firstWhere((element) => element.x ==  startNode.x && element.y == startNode.y);
+    final pathLength = pathFinderCopy.aStar(startNodeInCopy);
+    if (pathLength.length < shortestPath && pathLength.isNotEmpty) {
+      shortestPath = pathLength.length - 1;
+    }
+  }
+
+  return takenPath.length - 1;
+}
+
+class PathFinder {
+  final List<Node> heightMap;
+  final List<Node> alternativeStartNodes;
+  final Node startNode;
+  final Node goalNode;
+
+  PathFinder(this.heightMap, this.alternativeStartNodes, this.startNode, this.goalNode);
+
+  factory PathFinder.createFromFileLines(List<String> fileLines) {
+    List<Node> heightMap = [];
+    List<Node> alternativeStartNodes = [];
+    late Node startNode;
+    late Node goalNode;
+
+    for (int y = 0; y < fileLines.length; y++) {
+      final lineSplit = fileLines[y].split('');
+
+      for (int x = 0; x < lineSplit.length; x++) {
+        final char = lineSplit[x];
+
+        if (char == 'S') {
+          final newStartNode = Node('a'.codeUnits[0], x, y);
+          heightMap.add(newStartNode);
+          startNode = newStartNode;
+        } else if (char == 'E') {
+          goalNode = Node('z'.codeUnits[0], x, y);
+          heightMap.add(goalNode);
+        } else if (char == 'a') {
+          final newStartNode = Node('a'.codeUnits[0], x, y);
+          heightMap.add(newStartNode);
+          alternativeStartNodes.add(newStartNode);
+        }
+        else {
+          heightMap.add(Node(char.codeUnits[0], x, y));
+        }
+      }
+    }
+
+    for (final node in heightMap) {
+      final allNeighbors = heightMap.where((element) =>
+      element.isNeighbor(node) &&
+          ((node.height - element.height).abs() <= 1 || node.height > element.height));
+      node.neighbors.addAll(allNeighbors);
+    }
+
+    return PathFinder(heightMap, alternativeStartNodes, startNode, goalNode);
+  }
+
+  List<Node> aStar(Node startNode) {
+    final path = HashSet<Node>();
+    final frontier = HashSet<Node>()..add(startNode);
+    final explored = HashSet<Node>();
+
+    while (frontier.isNotEmpty) {
+      final node = frontier.reduce((current, next) => current.searchValue < next.searchValue ? current : next);
+      frontier.remove(node);
+      if (node == goalNode) {
+        var backTrack = node;
+        while (backTrack != startNode) {
+          path.add(backTrack);
+          backTrack = backTrack.cameFrom!;
+        }
+        path.add(startNode);
+        return path
+            .toList()
+            .reversed
+            .toList();
+      }
+      explored.add(node);
+      for (final nextNode in node.neighbors) {
+        final costSoFar = node.costSoFar + node.heuristic(nextNode);
+
+        if (explored.contains(nextNode)) {
+          continue;
+        }
+
+        if (!frontier.add(nextNode) && costSoFar >= nextNode.costSoFar) {
+          continue;
+        }
+
+        nextNode.cameFrom = node;
+        nextNode.costSoFar = costSoFar;
+        nextNode.searchValue =
+            nextNode.costSoFar + nextNode.heuristic(goalNode);
+      }
+    }
+
+    return path.toList();
+  }
+}
+
+class SearchNode {
+  SearchNode? cameFrom;
+  double costSoFar = 0;
+  double searchValue = 0;
+  Node node;
+  final List<SearchNode> neighbors;
+
+  SearchNode(this.node, this.neighbors);
+
+  double heuristic(SearchNode otherNode) {
+    final newX = (node.x - otherNode.node.x).abs();
+    final newY = (node.y - otherNode.node.y).abs();
+    return sqrt(newX * newX + newY * newY);
+  }
+}
+
+class Node {
+  final int height;
+  final int x;
+  final int y;
+  final List<Node> neighbors = [];
+  Node? cameFrom;
+  double costSoFar = 0;
+  double searchValue = 0;
+
+  Node(this.height, this.x, this.y);
+
+  bool isNeighbor(Node otherNode) {
+    return (otherNode.x == x - 1 && otherNode.y == y) ||
+        (otherNode.x == x + 1 && otherNode.y == y) ||
+        (otherNode.x == x && otherNode.y == y + 1) ||
+        (otherNode.x == x && otherNode.y == y - 1);
+  }
+
+  double heuristic(Node otherNode) {
+    final newX = (x - otherNode.x).abs();
+    final newY = (y - otherNode.y).abs();
+    return sqrt(newX * newX + newY * newY);
+  }
+}
