977e3fce2c0636f4ac1904d519d17d4a268fe7af
Philip S <philip.ppp@live.se>
2022-12-29 09:11:39 +0100

    day 18 optimize

--- lib/day_18/day_18.dart
+++ lib/day_18/day_18.dart
@@ -21,14 +21,16 @@ int _day18Read(List<String> fileLines, bool part2) {
   List<ThreeDObject> generateSearchableSpace() {
     final objectsInSpace = <ThreeDObject>[];
 
-    for (var x = 0; x < 25; x++) {
-      for (var y = 0; y < 25; y++) {
-        for (var z = 0; z < 25; z++) {
+    final cubesByX = cubes.sorted((a, b) => a.x.compareTo(b.x)).map((e) => e.x);
+    final cubesByY = cubes.sorted((a, b) => a.y.compareTo(b.y)).map((e) => e.y);
+    final cubesByZ = cubes.sorted((a, b) => a.z.compareTo(b.z)).map((e) => e.z);
+
+    for (var x = cubesByX.first - 2; x < cubesByX.last + 2; x++) {
+      for (var y = cubesByY.first - 2; y < cubesByY.last + 2; y++) {
+        for (var z = cubesByZ.first - 2; z < cubesByZ.last + 2; z++) {
           final cubeAtPosition = cubes.firstWhereOrNull(
               (element) => element.x == x && element.y == y && element.z == z,);
-          if (cubeAtPosition != null) {
-            objectsInSpace.add(cubeAtPosition);
-          } else {
+          if (cubeAtPosition == null) {
             objectsInSpace.add(_EmptySpace(x, y, z));
           }
         }
@@ -38,10 +40,11 @@ int _day18Read(List<String> fileLines, bool part2) {
     return objectsInSpace;
   }
 
+  final inPathToGoal = Set<ThreeDObject>();
   final notInPathToGoal = Set<ThreeDObject>();
 
-  bool isExteriorCube(_Cube start, List<ThreeDObject> searchSpace) {
-    final queue = PriorityQueue<ThreeDObject>((a, b) => a.distanceTo(_EmptySpace(0, 0, 0)).toInt() - b.distanceTo(_EmptySpace(0, 0, 0)).toInt());
+  bool isExteriorCube(ThreeDObject start, ThreeDObject goal, List<ThreeDObject> searchSpace) {
+    final queue = PriorityQueue<ThreeDObject>((a, b) => a.distanceTo(goal).toInt() - b.distanceTo(goal).toInt());
     // Create a set to store the visited cubes
     final visited = Set<ThreeDObject>();
     // Add the start cube to the queue
@@ -54,18 +57,16 @@ int _day18Read(List<String> fileLines, bool part2) {
       // Mark the current cube as visited
       visited.add(current);
       // Check if the current cube is the goal
-      if (current.x == 0 && current.y == 0 && current.z == 0) {
-        notInPathToGoal.removeAll(visited);
+      if (current == goal || inPathToGoal.contains(current)) {
+        inPathToGoal.addAll(visited);
         return true;
-      }
-
-      if (notInPathToGoal.contains(current)) {
+      } else if (notInPathToGoal.contains(current)) {
         break;
       }
 
       // Get the neighbors of the current cube
       final neighbors =
-          searchSpace.where((element) => current.isConnected(element) && element is _EmptySpace);
+          searchSpace.where((element) => current.isConnected(element));
       // Add the unvisited neighbors to the queue
       for (final neighbor in neighbors) {
         if (!visited.contains(neighbor)) {
@@ -79,16 +80,18 @@ int _day18Read(List<String> fileLines, bool part2) {
 
   var exposedSides = 0;
   final searchSpaces = generateSearchableSpace();
+  final goal = searchSpaces.firstWhere((element) => element.x == 0 && element.y == 0 && element.z == 0);
+  final externalSearchSpaces = searchSpaces.where((element) => isExteriorCube(element, goal, searchSpaces));
 
   for (final cube in cubes) {
     if (part2) {
-      final isExterior = isExteriorCube(cube, searchSpaces);
-      if (!isExterior) {
-        continue;
-      }
+      exposedSides += externalSearchSpaces.where((element) => cube.isConnected(element)).length;
     }
-
-    exposedSides += 6 - cubes.where((cube2) => cube.isConnected(cube2)).length;
+    else {
+      exposedSides += 6 - cubes
+          .where((cube2) => cube.isConnected(cube2))
+          .length;
+    }
   }
 
   return exposedSides;
