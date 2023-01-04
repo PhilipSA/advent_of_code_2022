1f47b164650743e629c2caa3db6e5fe7771cd2fb
Philip S <philip.ppp@live.se>
2023-01-04 19:46:31 +0100

    day20 part2

--- lib/day_20/day_20.dart
+++ lib/day_20/day_20.dart
@@ -6,38 +6,53 @@ void day20(IResultReporter resultReporter) {
   final fileLines = getInputFileLines(20);
 
   final part1 = _day20Read(fileLines, false);
-  final part2 = 2; //_day20Read(fileLines, true);
+  final part2 = _day20Read(fileLines, true);
 
   resultReporter.reportResult(20, part1, part2);
 }
 
 int _day20Read(List<String> fileLines, bool part2) {
   var numbers = fileLines
-      .mapIndexed((index, e) => _NumberWrapper(index, int.parse(e)))
+      .mapIndexed(
+        (index, e) => _NumberWrapper(
+          index,
+          int.parse(e) * (part2 ? 811589153 : 1),
+        ),
+      )
       .toList();
   final numbersCopy = numbers.toList();
+  final numberOfMixes = part2 ? 10 : 1;
 
-  for (final number in numbersCopy) {
-    final numbersReversed =
-        number.number < 0 ? numbers.reversed.toList() : numbers;
-    final index = numbersReversed
-        .indexWhere((element) => element.originalIndex == number.originalIndex);
-    final newIndex = index + number.number.abs();
+  for (var i = 0; i < numberOfMixes; ++i) {
+    for (final number in numbersCopy) {
+      final numbersReversed =
+      number.number < 0 ? numbers.reversed.toList() : numbers;
+      final index = numbersReversed
+          .indexWhere((element) =>
+      element.originalIndex == number.originalIndex);
+      final newIndex = index + number.number.abs();
 
-    numbersReversed
-      ..removeAt(index)
-      ..insert(newIndex % (numbersCopy.length - 1), number);
+      numbersReversed
+        ..removeAt(index)
+        ..insert(newIndex % (numbersCopy.length - 1), number);
 
-    if (number.number < 0) {
-      numbers = numbersReversed.reversed.toList();
+      if (number.number < 0) {
+        numbers = numbersReversed.reversed.toList();
+      }
     }
   }
 
   final number0Index = numbers.indexWhere((element) => element.number == 0);
 
-  return numbers.elementAt((number0Index + 1000) % (numbersCopy.length - 1)).number +
-      numbers.elementAt((number0Index + 2000) % (numbersCopy.length - 1)).number +
-      numbers.elementAt((number0Index + 3000) % (numbersCopy.length - 1)).number;
+  return numbers
+          .elementAt((number0Index + 1000) % (numbersCopy.length - 1))
+          .number +
+      numbers
+          .elementAt((number0Index + 2000) % (numbersCopy.length - 1))
+          .number +
+      numbers
+          .elementAt((number0Index + 3000) % (numbersCopy.length - 1))
+          .number;
 }
 
 class _NumberWrapper {
